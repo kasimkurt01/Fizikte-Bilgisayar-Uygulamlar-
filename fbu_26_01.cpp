ac341b8423c35c6fbf13d3f2810ca28b423623f8
kasimkurt01 <101169075+kasimkurt01@users.noreply.github.com>
2026-02-09 14:21:35 +0300

    Add files via upload

---
+++ fbu_26_01.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+double const PI=3.141593;
+int main(){
+long double h=1.0;
+long double x=PI/6;
+long double t,tt;
+	setlocale(LC_ALL,"TURKISH");
+	tt=cos(x);	
+	t=(sin(x+h)-sin(x))/h; //ileri türev
+	cout << setw(15)<<"h deðeri"<<setw(15)<<"turev" 
+	<< setw(15)<<"Tam Türev "<<setw(15)<<"Hata\n";
+	
+	cout <<  setw(15)<<h << setw(15) <<t << 
+	 setw(15)<< tt<< setw(15)<< tt-t<< "\n";
+	 
+	 while (h>1e-20){
+	 	h=h/10;
+	 	t=(sin(x+h)-sin(x))/h;
+	 	cout <<  setw(15)<<h << setw(15) <<t << 
+	 setw(15)<< tt<< setw(15)<< tt-t<< "\n";
+	 	
+	 }
+	
+}
